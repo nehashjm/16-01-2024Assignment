e5931fb4a8e3acdf849101a5ef4bc99bd4834d29
Nehash <nehash.j.m027@gmail.com>
2024-01-16 16:12:28 +0530

    Day8assignment

---
+++ WebApplicationbook/Models/Book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApplicationbook.Models
+{
+    public partial class Book
+    {
+        public int Bookid { get; set; }
+        public string? Author { get; set; }
+        public string? Publisher { get; set; }
+        public string? Category { get; set; }
+        public int? Price { get; set; }
+    }
+}
---
+++ WebApplicationbook/Models/BookStoredbContext.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace WebApplicationbook.Models
+{
+    public partial class BookStoredbContext : DbContext
+    {
+        public BookStoredbContext()
+        {
+        }
+
+        public BookStoredbContext(DbContextOptions<BookStoredbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Book> Books { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=tcp:bookservernehaash027.database.windows.net,1433;Initial Catalog=BookStoredb;Persist Security Info=False;User ID=nehashjmadmin;Password=neha@123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>(entity =>
+            {
+                entity.ToTable("book");
+
+                entity.Property(e => e.Bookid)
+                    .ValueGeneratedNever()
+                    .HasColumnName("bookid");
+
+                entity.Property(e => e.Author).HasMaxLength(50);
+
+                entity.Property(e => e.Category).HasMaxLength(50);
+
+                entity.Property(e => e.Publisher)
+                    .HasMaxLength(50)
+                    .HasColumnName("publisher");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
